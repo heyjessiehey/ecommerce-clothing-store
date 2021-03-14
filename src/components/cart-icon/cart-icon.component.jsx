c2a323aad871fc5d39076e836044bb9fee6fcd7d
heyjessiehey <jessieko608@gmail.com>
2021-03-14 16:01:09 -0400

    updating app with reselect, replaced cart selectors with memoized selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { toggleCartHidden } from "../redux/cart/cart.actions";
+import { selectCartItemCount } from "../redux/cart/cart.selectors";
 
 import { ReactComponent as Shoppingicon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <Shoppingicon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  // passing a whole reducer state into the selector
+  itemCount: selectCartItemCount(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/components/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from "reselect";
+
+// input selector - doesn't create selector
+const selectCart = (state) => state.cart; // get the whole state and return a slice of the state
+
+// output selector - use input selectors and create selector to build themseleves
+export const selectCartitems = createSelector(
+  [selectCart], // collection of array which is selectCart from input selector
+  (cart) => cart.cartItems // a function that is the value we want to return out of the selector
+);
+
+export const selectCartItemCount = createSelector(
+  [selectCartitems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulator, currentElement) => accumulator + currentElement.quantity,
+      0
+    )
+);
