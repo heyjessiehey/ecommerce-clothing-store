f0c542dd95d013f3ec12c99d30f6ebd10efdd9eb
heyjessiehey <jessieko608@gmail.com>
2021-03-13 20:23:41 -0500

    created cart-item component and connected cartdropdown with cartItems reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/preview-collection/collection-preview.component';
+import React from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
 
 class ShopPage extends React.Component {
-  constructor(){ // class field declaration
+  constructor() {
+    // class field declaration
     super();
 
     this.state = {
-      collections: SHOP_DATA
-    }
+      collections: SHOP_DATA,
+    };
   }
 
-  render(){ 
-    const {collections} = this.state; // destructor
+  render() {
+    const { collections } = this.state; // destructor
     return (
-      <div className='shop-page'>
-      <h1>Collections</h1>
-      {
-        collections.map(({id, ...otherCollectionProps}) => ( //section props
-          <CollectionPreview key ={id} {...otherCollectionProps} />
-        ))
-      }
+      <div className="shop-page">
+        <h1>Collections</h1>
+        {collections.map((
+          { id, ...otherCollectionProps } //section props
+        ) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
       </div>
     );
   }
